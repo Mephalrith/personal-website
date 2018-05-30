1d3ef2f2e02023af3f1bdeddc142f49f6db62c32
mephalrith <xsadrithx@yahoo.com>
2018-05-29 20:08:43 -0700

    Added about page.

--- src/pages/about.page.js
+++ src/pages/about.page.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import AppBar from "../components/appBar.component";
+import aboutHeader from '../assets/images/aboutHeader.svg';
 
 class AboutPage extends Component {
     render() {
@@ -8,8 +9,41 @@ class AboutPage extends Component {
 
                 <AppBar/>
 
-                <div>
-                    <p>About Page</p>
+                <div className={'about-body'}>
+                    <img src={aboutHeader}
+                         alt={"My name is Brooke Porter"}
+                         className={'about-header'}/>
+                    <div className={'about-text-background'}>
+                        <p className={'about-text'}>
+                            I do some stuff. I'll tell you all about my life right here. I guess.<br/><br/>
+                            After a short commercial break...<br/><br/>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/><br/>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/><br/>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/><br/>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            <br/><br/>
+                        </p>
+                    </div>
                 </div>
             </div>
         );
