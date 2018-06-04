028d5359b2d1d17724c26949ea3081d528b9225a
mephalrith <xsadrithx@yahoo.com>
2018-06-04 12:04:53 -0700

    About page text.

--- src/pages/about.page.js
+++ src/pages/about.page.js
@@ -26,21 +26,26 @@ class AboutPage extends Component {
                                  className={'about-header'}/>
                     }
                     <p className={'about-text'}>
-                        I do some stuff. I'll tell you all about my life right here. I guess.<br/><br/>
-                        After a short commercial break...<br/><br/>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/><br/>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-                        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        I am a developer with experience in graphic design and computer animation. I can make assets and
+                        also code!
                         <br/><br/>
+                        I went to Full Sail University in Winter Park, Florida for my BS in Computer Animation, but was
+                        unable to find a job with that degree when I returned home to Santa Rosa, California. I decided
+                        to return to school for Computer Science. I found that I enjoyed programming more than I ever
+                        enjoyed animation, and began learning languages on my own outside of class work.
+                        <br/><br/>
+                        I am very fluent in using Photoshop and Maya, and have experience with Illustrator and
+                        other software such as Inkscape, Sketchbook, and Paint Tool Sai.
+                        <br/><br/>
+                        Working in frontend development has been a very interesting experience for me, because I
+                        already had exposure to art and design before learning to program. I am able to make my own
+                        designs, create the assets, and then build the UI myself.
+                        <br/><br/>
+                        Because of that, I am particularly looking forward to working on backend and full stack
+                        applications, since I already have experience with frontend.
+                        <br/><br/>
+                        Contact me! I'd love to work with you.
+                        <br/>
                     </p>
                     {
                         this.props.theme === "Dark" ?
